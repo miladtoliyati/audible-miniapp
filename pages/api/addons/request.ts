6b9f303529d522e8774af7c61cf57458ab2b7a9e
Milad Toliyati <toliatimilad@gmail.com>
2025-10-29 23:33:40 -0400

    Update import path for buildX402Uri function

--- pages/api/addons/request.ts
+++ pages/api/addons/request.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { buildX402Uri } from '../../lib/x402';
+import { buildX402Uri } from '@lib/x402';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).end();
